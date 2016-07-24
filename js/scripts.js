6290da6ffbed7493cbb63ac4c2165e239c2978bd
Schoettker <lennartschoettker@hotmail.com>
2016-07-25 00:37:58 +0200

    fetch the values of the input form and display the result beneath

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// Business (backend) logic
 var add = function(number1, number2){
 	return number1 + number2;
 };
@@ -10,6 +11,14 @@ var multiply = function(number1, number2){
 var divide = function(number1, number2){
 	return number1 / number2;
 };
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
-alert(add(number1, number2));
+
+// User Interface (Front End) logic
+$(function(){
+	$("#add").submit(function(event){
+		event.preventDefault();
+		var number1 = parseInt($("#add1").val());
+		var number2 = parseInt($("#add2").val());
+		var result = add(number1, number2);
+		$("#result").text(result);
+	});
+});
