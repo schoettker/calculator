ae34aa96fc2084361160875e0e4b9c511fa824ae
Schoettker <lennartschoettker@hotmail.com>
2016-07-26 14:23:36 +0200

    delete js for every operator and add universal js which gathers input1 and input2

--- js/scripts.js
+++ js/scripts.js
@@ -15,36 +15,11 @@ var divide = function(number1, number2){
 // User Interface (Front End) logic
 $(function(){
 	// Add
-	$("#add").submit(function(event){
+	$("#calculator").submit(function(event){
 		event.preventDefault();
-		var number1 = parseInt($("#add1").val());
-		var number2 = parseInt($("#add2").val());
-		var result = add(number1, number2);
-		$("#resultAdd").text(result);
+		var number1 = parseInt($("#input1").val());
+		var number2 = parseInt($("#input2").val());
+		var result;
+		$("#output").text("Result: " + result);
 	});
-	// Subtract
-	$("#subtract").submit(function(event){
-		event.preventDefault();
-		var number1 = parseInt($("#subtract1").val());
-		var number2 = parseInt($("#subtract2").val());
-		var result = subtract(number1, number2);
-		$("#resultSubtract").text(result);
-	});
-	// Multiply
-	$("#multiply").submit(function(event){
-		event.preventDefault();
-		var number1 = parseInt($("#multiply1").val());
-		var number2 = parseInt($("#multiply2").val());
-		var result = multiply(number1, number2);
-		$("#resultMultiply").text(result);
-	});
-	// Divide
-	$("#divide").submit(function(event){
-		event.preventDefault();
-		var number1 = parseInt($("#divide1").val());
-		var number2 = parseInt($("#divide2").val());
-		var result = divide(number1, number2);
-		$("#resultDivide").text(result);
-	});
-
 });
