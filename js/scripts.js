b0e3999b96ec1b3f81ef6ec5ef0426407b90b861
Schoettker <lennartschoettker@hotmail.com>
2016-07-26 14:28:52 +0200

    add branches for different operators

--- js/scripts.js
+++ js/scripts.js
@@ -19,7 +19,17 @@ $(function(){
 		event.preventDefault();
 		var number1 = parseInt($("#input1").val());
 		var number2 = parseInt($("#input2").val());
+		var operator = $("input:radio[name=operator]:checked").val();
 		var result;
+		if (operator === "add") {
+			result = add(number1, number2);
+		} else if (operator === "subtract") {
+			result = subtract(number1, number2);
+		} else if (operator === "multiply") {
+			result = multiply(number1, number2);
+		} else if (operator === "divide") {
+			result = divide(number1, number2);
+		}
 		$("#output").text("Result: " + result);
 	});
 });
